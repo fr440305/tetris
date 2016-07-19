290d10d6beb3a0e239816c66a4a8e0c8f93b7e77
LBT <lbt@LBTdeMacBook.local>
2016-07-18 17:43:40 -0700

    Realize the square_container.

---
+++ Processor.js
@@ -0,0 +1,42 @@
+/*  file:
+				name: Processor.js;
+				mker: fire_rain(alone);
+*/
+
+var Processor = {
+	is_dead: 0,
+	eventLoad: function () {
+		var square_container = document.getElementById ("_square_container");
+		//初始化表格
+		var i = 0, j = 0;
+		for (i = 0; i <= 14; i++) {
+			container_row = square_container.insertRow (i);
+			for (j = 0; j <= 9; j++) {
+				container_cell = container_row.insertCell (j);
+				container_cell.style.width = "10px";
+				container_cell.style.height = "10px";
+				container_cell.style.backgroundColor = "#000000";
+
+			}
+		}
+		//再调整一下表格的样式
+		square_container.style.margin = "auto";
+	},
+
+	eventResize: function () {
+		
+	},
+
+
+	gameMainLoop: function () {
+		Processor.eventLoad ();
+		
+	}
+
+};
+
+/* Game - - Start! */
+
+alert("111");
+Processor.gameMainLoop ();
+
